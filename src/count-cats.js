ad5bf75bd72dbe4157d2b422be9f8f25d5fcdb5b
Slava Torchik <torhik_slava@mail.ru>
2020-02-19 21:29:59 +0300

    feat: count the cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,9 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let cats = 0;
+  for(let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === '^^') cats++;
+    }
+  }
+  return cats;
 };
