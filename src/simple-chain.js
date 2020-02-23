1ef53f2616ce89c2dbe7167edca82b2cb24ddc77
Slava Torchik <torhik_slava@mail.ru>
2020-02-23 19:03:02 +0300

    feat: implement solution for simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,29 @@
 const chainMaker = {
+  chain: [],
+  
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (this.chain[position-1] === undefined) {
+      this.chain = [];
+      throw new Error('Invalid position');
+    }
+    this.chain.splice(position-1, 1);
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let result = this.chain.join('~~');
+    this.chain = [];
+    return result;
   }
 };
 
