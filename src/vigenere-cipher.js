1b7d2fae907ad15d51cf03f9f5be5db4fe56d85e
Slava Torchik <torhik_slava@mail.ru>
2020-02-25 22:49:05 +0300

    feat: create crypt-machine!

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,63 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+	constructor (mode = true) {
+		this.direct_mode = mode;
+	}
+	_init (message, key) {
+		if(!message || !key) throw new Error('Not all params entered!');
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+		if (key.length>=message.length) {
+			key = key.slice(0, message.length);
+		} else {
+			let mult = Math.floor(message.length/key.length);
+			while (mult>0) {
+				key +=key;
+				mult--;
+			}
+			key = key.slice(0, message.length);
+		}
+		this.key = key.toUpperCase();
+		this.message = message.toUpperCase();
+		this.result = '';
+		this.lag = 0;
+	}
+	
+	_is_letter(index) {		
+		if(this.message.codePointAt(index) < 65 || this.message.codePointAt(index) > 90) {
+			this.result += this.message[index];			
+			this.lag --;																	 
+			return false;
+		} else {return true;}
+	}
+
+  encrypt(message, key) {
+		this._init(message, key);
+		for (let i = 0; i < this.message.length; i++){
+			if(!this._is_letter(i)) continue;
+			let encPos = this.key.codePointAt(i + this.lag) + (this.message.codePointAt(i) - 65);		
+			encPos > 90 ? encPos -= 26 : encPos;		
+			this.result += String.fromCodePoint(encPos);
+		}
+		if(this.direct_mode) {
+			return this.result;
+		} else {
+			return this.result.split('').reverse().join('');
+		}
+  }
+
+  decrypt(message, key) {
+    this._init(message, key);
+		for (let i = 0; i < this.message.length; i++){
+			if(!this._is_letter(i)) continue;
+			let decPos = 65 + (this.message.codePointAt(i) - this.key.codePointAt(i + this.lag));		
+			decPos < 65 ? decPos += 26 : decPos;		
+			this.result += String.fromCodePoint(decPos);
+		}
+		if(this.direct_mode) {
+			return this.result;
+		} else {
+			return this.result.split('').reverse().join('');
+		}
+  }
 }
 
 module.exports = VigenereCipheringMachine;
