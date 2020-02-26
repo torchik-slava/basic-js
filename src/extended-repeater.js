79f942cade915d16ea43eaa75e8ac8883f11702b
Slava Torchik <torhik_slava@mail.ru>
2020-02-26 20:13:39 +0300

    feat: implement extended-repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,25 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+	let result = '' + str,
+	repeatTimes = options.repeatTimes,
+	separator = options.separator || '+',
+	addition = options.addition,         
+	additionRepeatTimes = options.additionRepeatTimes,
+	additionSeparator = options.additionSeparator || '|';	
+
+	if(options.hasOwnProperty('addition')) result += addition; 
+	
+	while (additionRepeatTimes > 1) {
+		result += additionSeparator + addition;
+		additionRepeatTimes--;
+	}
+	
+	if(repeatTimes>1) {
+		let repeatPart = separator + result;
+		while(repeatTimes>1) {
+			result += repeatPart;
+			repeatTimes--; 
+		}
+	}
+	return result;
 };
   
